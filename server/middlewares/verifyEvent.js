7e87607c40b403e7f76c57d85178ca720eb2e075
MariaApa <mluquelopezwrk@gmail.com>
2023-06-02 13:46:13 +0200

    eventos

--- server/middlewares/verifyEvent.js
+++ server/middlewares/verifyEvent.js
@@ -8,33 +8,20 @@ const User = db.user;
 
 checkIfEventExists = (req, res, next) => {
   Event.findOne({
- 
     date: req.body.date,
     author: req.body.author
   })
   .exec()
   .then(event => {
     if (event) {
-      res.status(400).send({ message: "Error, ya existe un evento con esa fecha y autor" });
+      res.status(400).send({ message: "Error, ya existe un evento para esta fecha y creador" });
       return;
     }
     
     next();
   })
   .catch(err => {
-    res.status(500).send({ message: err });
+    res.status(500).send({ message: err.message });
     return;
   });
 };
-  
-  
-  module.exports = checkIfEventExists;
-
-
-
-const verifyEvent = {
-    checkIfEventExists,
-  
-};
-
-module.exports = verifyEvent;
